5212473e03e18ca5391c9fc4a18969b57c57021b
westelh <westelh@gmail.com>
2024-03-08 13:29:12 +0900

    Add homebrew(linux)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,8 +20,13 @@ end
 set -gx LESSHISTFILE '-'
 
 # Homebrew
+# MacOS
 if test -f /opt/homebrew/bin/brew
   eval (/opt/homebrew/bin/brew shellenv)
+end
+# Linux
+if test -f /home/linuxbrew/.linuxbrew/bin/brew
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
 # K9s command completion
