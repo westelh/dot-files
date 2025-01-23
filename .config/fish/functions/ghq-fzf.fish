f3632196cc1a611fa5b887f193cd3b8b442e56df
Taisei Nishiumi <westelh@gmail.com>
2025-01-24 00:23:07 +0900

    fish:ghq-fzf関数がバッファラインを更新するように変更

--- .config/fish/functions/ghq-fzf.fish
+++ .config/fish/functions/ghq-fzf.fish
@@ -1,6 +1,6 @@
 function ghq-fzf
   set -l src (ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
   if test -n "$src"
-    cd (ghq root)/$src
+    cd (ghq root)/$src; commandline -f repaint
   end
 end
