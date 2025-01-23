c5c2f47b25a18cb71c81134228991bbfcad2f07e
Taisei Nishiumi <westelh@gmail.com>
2025-01-23 12:10:31 +0900

    nvimにLSPプラグインを導入

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,7 +1,12 @@
 return {
   "hrsh7th/nvim-cmp",
+  lazy = false,
   dependencies = {
+    -- Buffer
     "hrsh7th/cmp-buffer",
+    -- LSP
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
     local cmp = require'cmp'
@@ -9,14 +14,21 @@ return {
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-1>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+      }, {
         { name = 'buffer' },
       }),
     })
+
+    -- LSPCONFIG SETUP
+    -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    require'lspconfig'.lua_ls.setup({})
   end,
 }
 
