900ec6d8590b8fb38a882b4fb458aa08151534e0
Taisei Nishiumi <westelh@gmail.com>
2025-01-24 00:49:16 +0900

    nvim:プラグイン更新およびActions設定

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -65,6 +65,9 @@ return {
     -- LSPCONFIG SETUP
     -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    require'lspconfig'.gh_actions_ls.setup({
+      capabilities = capabilities,
+    })
     require'lspconfig'.lua_ls.setup({
       capabilities = capabilities,
       on_init = function (client)
