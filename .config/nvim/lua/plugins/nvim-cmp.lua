0c79d72d13908d5f22e3bf3fb96feea61643e3a3
Taisei Nishiumi <westelh@gmail.com>
2025-01-25 20:18:34 +0900

    nvimのLSP設定にTerraform LSを追加

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -65,6 +65,7 @@ return {
     -- LSPCONFIG SETUP
     -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    require'lspconfig'.terraformls.setup{}
     require'lspconfig'.gh_actions_ls.setup({
       capabilities = capabilities,
     })
