34d48f68af14fb0ba0c58eda039f41b8905e16a3
Taisei Nishiumi <westelh@gmail.com>
2025-01-29 18:21:06 +0900

    LazyLuaを遅延読み込み

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,6 +1,7 @@
 return {
   "L3MON4D3/LuaSnip",
   version = "v2.*",
+  lazy = true,
   build = "make install_jsregexp",
   dependencies = { "rafamadriz/friendly-snippets" },
   config = function ()
