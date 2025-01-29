3b21ea216ec472b54210352957e72e32069c0728
Taisei Nishiumi <westelh@gmail.com>
2025-01-29 19:45:47 +0900

    nvimのtoggletermプラグインにlazygitの設定を追加

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,10 +1,23 @@
+local lazygit = nil
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
   opts = {
     open_mapping = [[<c-\>]],
   },
+  init = function ()
+    lazygit = require'toggleterm.terminal'.Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      hidden = true,
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+    })
+  end,
   keys = {
-    { [[<c-\>]] }
+    { [[<c-\>]] },
+    { "<leader>g", function() lazygit:toggle() end },
   }
 }
