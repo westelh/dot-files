1b648595695e5301532a75f5f6f583b4e1c272d9
Taisei Nishiumi <westelh@gmail.com>
2025-06-03 22:57:54 +0900

    Update fish function ghq-fzf

--- .config/fish/functions/ghq-fzf.fish
+++ .config/fish/functions/ghq-fzf.fish
@@ -1,9 +1,16 @@
 function ghq-fzf
   set -l src (ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+
   if test -n "$src"
+    set -l dest (ghq root)/$src
     set -l name (string replace -a '.' '_' $src)
-    tmux new-session -d -s $name -c (ghq root)/$src fish
-    tmux attach-session -t $name
+
+    if test -n "$TMUX"
+      tmux split-window -h -c $dest
+    else
+      tmux new-session -d -s $name -c $dest fish
+      tmux attach-session -t $name
+    end
     commandline -f repaint
   end
 end
