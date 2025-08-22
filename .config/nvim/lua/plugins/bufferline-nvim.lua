be9fdf0eb3e0c9cc863f8b8a4d5e9eedddcad65b
Taisei Nishiumi <westelh@gmail.com>
2025-08-22 19:31:24 +0900

    Update configs of bufferline-nvim

--- .config/nvim/lua/plugins/bufferline-nvim.lua
+++ .config/nvim/lua/plugins/bufferline-nvim.lua
@@ -3,5 +3,18 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   lazy = false,
-  opts = {},
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = function(count, level, _, _)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
+      end,
+      hover = {
+        enabled = true,
+        delay = 200,
+        reveal = { 'close' },
+      }
+    }
+  },
 }
