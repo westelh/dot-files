cad46635daaf4db9dac94bed6c81d4a812833cf9
Taisei Nishiumi <westelh@gmail.com>
2025-08-25 20:29:04 +0900

    Change options order in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,12 @@
+require("config.lazy")
+require('config.sessions')
+require('config.lsp')
+require('config.neovide')
+require('config.keymap')
+require('config.folding')
+require('config.filetype')
+require('config.autocmd')
+
 vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
@@ -8,13 +17,4 @@ vim.o.list = true
 vim.o.ambiwidth = 'single'
 vim.o.conceallevel = 1
 vim.cmd 'syntax enable'
-
-require("config.lazy")
-require('config.sessions')
-require('config.lsp')
-require('config.neovide')
-require('config.keymap')
-require('config.folding')
-require('config.filetype')
-require('config.autocmd')
 
