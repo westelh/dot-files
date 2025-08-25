01202572ea079b94234192f004ecb69222687a44
Taisei Nishiumi <westelh@gmail.com>
2025-08-25 20:28:41 +0900

    Organize init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,32 +15,6 @@ require('config.lsp')
 require('config.neovide')
 require('config.keymap')
 require('config.folding')
+require('config.filetype')
+require('config.autocmd')
 
-vim.filetype.add({
-  extension = {
-    -- Map .tf to terraform file
-    -- No need to treat it as TF MUD file
-    tf = "terraform"
-  },
-  pattern = {
-    [".*/templates/.*%.yaml"] = "helm",
-    [".*/templates/.*%.tpl"] = "helm",
-  },
-})
-
-vim.api.nvim_create_autocmd({ 'User' }, {
-  pattern = 'LuasnipChoiceNodeEnter',
-  callback = function()
-    vim.schedule(function()
-      vim.notify('LuasnipChoiceEnter')
-      require("luasnip.extras.select_choice")()
-    end)
-  end
-})
-
--- highlight yanked area
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-  end,
-})
