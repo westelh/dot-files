f90c7335bfeb972d821079e2c41eb140e746f935
Taisei Nishiumi <westelh@gmail.com>
2025-08-26 01:38:42 +0900

    Add shell param to toggleterm.nvim

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,10 @@
-local lazygit = nil
-
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
   opts = {
     open_mapping = [[<C-h>]],
-    direction = "float"
+    direction = "float",
+    shell = "fish"
   },
   keys = {
     { [[<C-h>]] },
