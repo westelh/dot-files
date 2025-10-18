32d0a5f6d2625bc8ad412acc0744c2c922bdd711
Taisei Nishiumi <westelh@gmail.com>
2025-10-18 21:34:21 +0900

    Enable Vue tooling in Neovim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -24,14 +24,43 @@ return {
     vim.lsp.config('lua_ls', {
       settings = {
         Lua = {
-          diagnostics = { globals = { "vim" }}
+          diagnostics = { globals = { "vim" } }
         }
       },
     })
     vim.lsp.enable('lua_ls')
 
+    local n_prefix = vim.env.N_PREFIX
+    local vue_language_server_path = n_prefix .. '/lib/node_modules/@vue/language-server'
+    local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+    local vue_plugin = {
+      name = '@vue/typescript-plugin',
+      location = vue_language_server_path,
+      languages = { 'vue' },
+      configNamespace = 'typescript',
+    }
+    local ts_ls_config = {
+      init_options = {
+        plugins = {
+          vue_plugin,
+        },
+      },
+      on_attach = function(client, bufnr)
+        -- フォーマット機能をオフにして競合防止
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      filetypes = tsserver_filetypes,
+    }
+    vim.lsp.config('ts_ls', ts_ls_config)
+
     -- Javascript and Typescript
     vim.lsp.enable('ts_ls')
+    -- Vue
+    vim.lsp.config('vue_ls', {})
+    vim.lsp.enable('vue_ls')
+
+    vim.lsp.config('eslint', {})
+    vim.lsp.enable('eslint')
   end
 }
-
--- .zprofile
+++ .zprofile
@@ -1,4 +1,8 @@
 # Added by Toolbox App
 export PATH="$PATH:/Users/taisei/Library/Application Support/JetBrains/Toolbox/scripts"
 
+export PATH="$PATH:/Users/taisei/.n/bin"
+
+export N_PREFIX="/Users/taisei/.n"
+
 eval $(/opt/homebrew/bin/brew shellenv)
