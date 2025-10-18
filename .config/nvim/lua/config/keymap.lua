ae1d0c90609c32fd070a24210982f1fd6cc7ccf7
Taisei Nishiumi <westelh@gmail.com>
2025-10-18 21:34:30 +0900

    Tweak Neovim navigation configs

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -5,23 +5,21 @@ vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>bprevious<cr>', { noremap = true, si
 vim.api.nvim_set_keymap('n', '<C-q>', '<cmd>bd<cr>', { noremap = true })
 
 local function open(it, fallback)
-  if it:match("^http://") or it:match("^https://") then
-    system.open(it)
-  else
-    fallback(it)
-  end
+	if it:match("^http://") or it:match("^https://") then
+		system.open(it)
+	else
+		fallback(it)
+	end
 end
 
 vim.keymap.set("n", "gf", function()
-  open(vim.fn.expand("<cfile>"), function()
-    vim.cmd("normal! gf")
-  end)
+	open(vim.fn.expand("<cfile>"), function()
+		vim.cmd("normal! gf")
+	end)
 end)
 
 vim.keymap.set("n", "gF", function()
-  open(vim.fn.expand("<cfile>"), function()
-    vim.cmd("normal! gf")
-  end)
+	open(vim.fn.expand("<cfile>"), function()
+		vim.cmd("normal! gf")
+	end)
 end)
-
-
--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -23,6 +23,15 @@ return {
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn", -- set to `false` to disable one of the mappings
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    }
   },
 
   config = function(_, opts)
