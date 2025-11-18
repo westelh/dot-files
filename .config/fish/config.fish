d93c1f5d54757d5223bb9c195ca62db8b17b1f9e
Taisei Nishiumi <westelh@gmail.com>
2025-11-18 11:44:24 +0900

    Tweak prompt configs and update nvim lock

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -76,3 +76,12 @@ bind -M insert ctrl-q ghq-fzf
 
 set PATH $HOME/.jenv/bin $PATH
 status --is-interactive; and jenv init - | source
+
+function starship_transient_prompt_func
+  starship module character
+end
+function starship_transient_rprompt_func
+  #starship module time
+end
+starship init fish | source
+enable_transience
