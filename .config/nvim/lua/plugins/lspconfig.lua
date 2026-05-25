ead6749a0ac39eeebaae5d0f5ee6d1fdb5b43faa
げ <westelh@gmail.com>
2026-05-25 15:12:53 +0900

    Add system nodejs fallback when N_PREFIX does not exists

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -30,8 +30,15 @@ return {
     })
     vim.lsp.enable('lua_ls')
 
-    local n_prefix = vim.env.N_PREFIX
-    local vue_language_server_path = n_prefix .. '/lib/node_modules/@vue/language-server'
+    local node_modules_path = nil
+    if vim.env.N_PREFIX then
+      -- node_modules_path is '$N_PREFIX/lib/node_modules' when $N_PREFIX exists
+      node_modules_path = vim.env.N_PREFIX .. '/lib/node_modules'
+    else
+      node_modules_path = '/usr/lib/node_modules'
+    end
+
+    local vue_language_server_path = node_modules_path .. '/@vue/language-server'
     local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
     local vue_plugin = {
       name = '@vue/typescript-plugin',
