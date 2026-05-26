05655dc595fa00899a782b27dce951ead54d49b7
げ <westelh@gmail.com>
2026-05-27 08:19:08 +0900

    Enable tree-sitter-manager-nvim

---
+++ .config/nvim/lua/plugins/tree-sitter-manager.lua
@@ -0,0 +1,11 @@
+-- https://github.com/romus204/tree-sitter-manager.nvim?utm_source=chatgpt.com
+-- Alternates core functionality of archived nvim-treesitter
+
+return {
+  'romus204/tree-sitter-manager.nvim',
+  lazy = false,
+  opts = {
+    -- hilight = { "lua", "c" },
+    -- nohighlight = { "lua", "c" },
+  }
+}
