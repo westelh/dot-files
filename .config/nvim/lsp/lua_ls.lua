5d6aed4c40957f2fe68756739620df0328109e63
げ <westelh@gmail.com>
2026-05-26 21:19:34 +0900

    Add library path to the lua_ls workspace

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -2,7 +2,22 @@ return {
   settings = {
     Lua = {
       -- 'vim'グローバル変数をセット
-      diagnostics = { globals = { "vim" } }
+      diagnostics = { globals = { "vim" } },
+
+      runtime = {
+        version = 'LuaJIT',
+        pathStrict = true,
+        path = { "?.lua", "?/init.lua" },
+      },
+
+      workspace = {
+        library = vim.list_extend(vim.api.nvim_get_runtime_file('lua', true), {
+          "${3rd}/luv/library",
+          "${3rd}/busted/library",
+          "${3rd}/luassert/library",
+        }),
+        checkThirdParty = 'Disable',
+      },
     }
   }
 }
